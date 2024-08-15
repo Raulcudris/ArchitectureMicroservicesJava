7fc6be420e6fe13ea0bbdfede16efb1075a0c487
RaulCudris <raulcudriz@hotmail.com>
2024-08-15 18:11:01 -0500

    Actualizacion de los microservicios en el Config-data

--- ConfigServicesApplication/src/main/java/com/shoppingmakiia/ConfigServicesApplication/ConfigServicesApplication.java
+++ ConfigServicesApplication/src/main/java/com/shoppingmakiia/ConfigServicesApplication/ConfigServicesApplication.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServicesApplication.class, args);
 	}
-
 }
---
+++ GatewayServicesApplication/src/main/java/com/shoppingmakiia/GatewayServicesApplication/Dto/RequestDto.java
@@ -0,0 +1,14 @@
+package com.shoppingmakiia.GatewayServicesApplication.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class RequestDto {
+    private String uri;
+    private String method;
+}
