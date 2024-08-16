4d93562a8715953b399619d1d0e34332559f2fa6
RaulCudris <raulcudriz@hotmail.com>
2024-08-16 17:05:09 -0500

    Actualizacion del Archivos ConfigData

--- AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Security/JwtProvider.java
+++ AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Security/JwtProvider.java
@@ -3,10 +3,13 @@ import com.shoppingmakiia.AuthServiceApplication.Dto.RequestDto;
 import com.shoppingmakiia.AuthServiceApplication.Entity.AuthRequest;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
@@ -17,31 +20,37 @@ public class JwtProvider {
 
     @Value("${jwt.secret}")
     private String secret;
+    @Value("${jwt.timeExpiration}")
+    private String timeExpiration;
     @Autowired
     RouteValidator routeValidator;
 
-    @PostConstruct
-    protected void init() {
-        secret = Base64.getEncoder().encodeToString(secret.getBytes());
-    }
+    //Generate token of access
     public String createToken(AuthRequest authRequest) {
         Map<String, Object> claims = new HashMap<>();
         claims = Jwts.claims().setSubject(authRequest.getUsername());
         claims.put("id", authRequest.getId());
         claims.put("role",authRequest.getRole());
         Date now = new Date();
-        Date exp = new Date(now.getTime() + 3600000);
+        Date exp = new Date(now.getTime() + timeExpiration);
+
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(exp)
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(getSignatureKey(),SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public boolean validate(String token, RequestDto dto) {
         try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(getSignatureKey()).parseClaimsJws(token);
+             /* Jwts.builder()
+                    .setSubject(username)
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis()+Long.parseLong(timeExpiration)))
+                    .signWith((java.security.Key) getSignatureKey(), SignatureAlgorithm.HS256)
+                    .compact();*/
         }catch (Exception e){
             return false;
         }
@@ -63,5 +72,9 @@ public class JwtProvider {
         return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody().get("role").equals("admin");
     }
 
+    public SecretKey getSignatureKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
 }
--- EurekaServicesApplication/src/main/java/com/shoppingmakiia/EurekaServicesApplication/EurekaServicesApplication.java
+++ EurekaServicesApplication/src/main/java/com/shoppingmakiia/EurekaServicesApplication/EurekaServicesApplication.java
@@ -2,6 +2,7 @@ package com.shoppingmakiia.EurekaServicesApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServicesApplication {
