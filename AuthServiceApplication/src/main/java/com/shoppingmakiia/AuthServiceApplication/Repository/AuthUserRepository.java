f8eb080bedd62f512105e7518c978ccca760e69a
RaulCudris <raulcudriz@hotmail.com>
2024-08-17 16:09:43 -0500

    Actualizacion del Archivos ConfigData

--- AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Repository/AuthUserRepository.java
+++ AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Repository/AuthUserRepository.java
@@ -1,11 +1,15 @@
 package com.shoppingmakiia.AuthServiceApplication.Repository;
 import com.shoppingmakiia.AuthServiceApplication.Entity.AuthRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
 public interface AuthUserRepository extends JpaRepository<AuthRequest, Integer> {
 
-    Optional<AuthRequest> findByUsername(String username);
+    String FILTER_USERNAME_QUERY = "SELECT c FROM EntyRecmaeagendahma c  WHERE c.recSecregReat  = :username";
+    @Query(value = FILTER_USERNAME_QUERY)
+    Optional<AuthRequest> findByUsername(@Param("username") String username);
 }
