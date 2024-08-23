503f7aaf23e21a9c1d62097499a768a0036833e8
RaulCudris <raulcudriz@hotmail.com>
2024-08-23 16:55:30 -0500

    Actualizacion y modificacion del ApiGateway y el AuthServices

--- AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Security/SecurityConfig.java
+++ AuthServiceApplication/src/main/java/com/shoppingmakiia/AuthServiceApplication/Security/SecurityConfig.java
@@ -1,13 +1,31 @@
 package com.shoppingmakiia.AuthServiceApplication.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtProvider jwtAuthenticationFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore((Filter) jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
+
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }*/
 }
--- GatewayServicesApplication/src/main/java/com/shoppingmakiia/GatewayServicesApplication/Config/AuthFilter.java
+++ GatewayServicesApplication/src/main/java/com/shoppingmakiia/GatewayServicesApplication/Config/AuthFilter.java
@@ -13,38 +13,44 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
-    private WebClient.Builder webClient;
-    public AuthFilter (WebClient.Builder webClient){
+    private final WebClient.Builder webClient;
+
+    public AuthFilter(WebClient.Builder webClient) {
         super(Config.class);
         this.webClient = webClient;
     }
+
     @Override
     public GatewayFilter apply(Config config) {
-        return (((exchange, chain) ->{
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
-                return onError(exchange, HttpStatus.BAD_REQUEST);
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, HttpStatus.UNAUTHORIZED);
             }
+
             String tokenHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-            String [] chunks = tokenHeader.split(" ");
-            if (chunks.length !=2 || !chunks[0].equals("Bearer")){
+            String[] chunks = tokenHeader.split(" ");
+            if (chunks.length != 2 || !chunks[0].equals("Bearer")) {
                 return onError(exchange, HttpStatus.BAD_REQUEST);
             }
+
             return webClient.build()
                     .post()
-                    .uri("http://Auth-Services/auth/validate?token="+chunks[1])
+                    .uri("http://Auth-Services/auth/validate?token=" + chunks[1])
                     .bodyValue(new RequestDto(exchange.getRequest().getPath().toString(), exchange.getRequest().getMethod().toString()))
-                    .retrieve().bodyToMono(TokenDto.class)
-                    .map( t->{
-                        t.getToken();
-                        return  exchange;
-                    }).flatMap(chain::filter);
-        }));
+                    .retrieve()
+                    .bodyToMono(TokenDto.class)
+                    .map(tokenDto -> {
+                        tokenDto.getToken();
+                        return exchange;
+                    })
+                    .flatMap(chain::filter)
+                    .onErrorResume(e -> onError(exchange, HttpStatus.INTERNAL_SERVER_ERROR));
+        };
     }
-
-    public Mono<Void> onError(ServerWebExchange exchange , HttpStatus status){
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(status);
-        return  response.setComplete();
+        return response.setComplete();
     }
     public static class Config {}
 }
